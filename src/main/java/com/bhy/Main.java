d94571785991b605fc5f841d80c3c0624a80c282
admin_bhy <736143842@qq.com>
2023-01-10 17:45:13 +0800

    hot-fix commit1

--- src/main/java/com/bhy/Main.java
+++ src/main/java/com/bhy/Main.java
@@ -1,9 +1,12 @@
 package com.bhy;
-
+/**
+ * @author bhy
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello git!");
         System.out.println("Hello github!");
+        System.out.println("Hello git-merge!");
     }
 }
