c6b25c002cc62ff193e55f741e44215e7658b3df
admin_bhy <736143842@qq.com>
2023-01-10 17:22:27 +0800

    second commit

--- src/main/java/com/bhy/Main.java
+++ src/main/java/com/bhy/Main.java
@@ -2,6 +2,7 @@ package com.bhy;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         System.out.println("Hello git!");
     }
 }
